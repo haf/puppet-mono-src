3a8873f4e54fbf66d343c493284c4e2acf7e79f0
Henrik Feldt <henrik@haf.se>
2013-05-27 13:48:56 +0200

    Update buildmono.sh
    Update with `set -e` and popd before running fpm.

--- files/buildmono.sh
+++ files/buildmono.sh
@@ -1,4 +1,5 @@
 # http://wiki.centos.org/HowTos/SetupRpmBuildEnvironment
+set -e
 VERSION=${0:-3.0.10}
 sudo yum -y install gcc mysql-devel ruby-devel rubygems rpm-build redhat-rpm-config gettext
 sudo gem install fpm --no-rdoc --no-ri
@@ -7,5 +8,6 @@ tar jxf mono-$VERSION.tar.bz2
 pushd mono-$VERSION
 ./configure --prefix=/opt/mono
 make
-make install DESTDIR=/tmp/mono-install
-sudo fpm -s dir -t rpm -n mono -v $VERSION -C /tmp/mono-install .
+make install DESTDIR=/tmp/mono-$VERSION
+popd
+sudo fpm -s dir -t rpm -n mono -v $VERSION -C /tmp/mono-$VERSION .
