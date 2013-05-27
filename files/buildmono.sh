fd8664bcfbb938668c2623dd2ff4cef27aa9ef68
Henrik Feldt <henrik@haf.se>
2013-05-27 13:47:50 +0200

    Update buildmono.sh
    Support versions and add all necessary steps

--- files/buildmono.sh
+++ files/buildmono.sh
@@ -1,7 +1,11 @@
 # http://wiki.centos.org/HowTos/SetupRpmBuildEnvironment
-sudo yum -y install gcc mysql-devel ruby-devel rubygems rpm-build redhat-rpm-config
-sudo gem install fpm
+VERSION=${0:-3.0.10}
+sudo yum -y install gcc mysql-devel ruby-devel rubygems rpm-build redhat-rpm-config gettext
+sudo gem install fpm --no-rdoc --no-ri
+curl -O --silent http://download.mono-project.com/sources/mono/mono-$VERSION.tar.bz2
+tar jxf mono-$VERSION.tar.bz2
+pushd mono-$VERSION
 ./configure --prefix=/opt/mono
 make
 make install DESTDIR=/tmp/mono-install
-sudo fpm -s dir -t rpm -n mono -v 3.0.6 -C /tmp/mono-install .
+sudo fpm -s dir -t rpm -n mono -v $VERSION -C /tmp/mono-install .
