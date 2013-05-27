cbe698bf01f7a4602657c6d4a5879fbd18004b79
Henrik Feldt <henrik@haf.se>
2013-05-27 13:58:08 +0200

    Update buildmono.sh
    Adding current head comment to the bash script.

--- files/buildmono.sh
+++ files/buildmono.sh
@@ -4,6 +4,7 @@ VERSION=${1:-3.0.10}
 sudo yum -y install gcc mysql-devel ruby-devel rubygems rpm-build redhat-rpm-config gettext
 sudo gem install fpm --no-rdoc --no-ri
 curl -O --silent http://download.mono-project.com/sources/mono/mono-$VERSION.tar.bz2
+# for current HEAD: curl -O -k -L https://github.com/mono/mono/tarball/master
 tar jxf mono-$VERSION.tar.bz2
 pushd mono-$VERSION
 ./configure --prefix=/opt/mono
